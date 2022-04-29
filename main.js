19156cddcf854efa9461a86d4c113e615cc8399d
Carlos E Alford <carlos.alford@gmail.com>
2022-04-29 10:37:02 +0100

    Task 5 complete

--- main.js
+++ main.js
@@ -1,40 +1,46 @@
-// Moves
-let playerMove = prompt("Type rock, paper, scissors");
-let computerMove = randomMove();
+let keepPlaying = true;
 
-function randomMove() {
-    let moves = ["rock", "paper", "scissors"]
-    let random = Math.floor(Math.random() * 2);
-    console.log(random);
-    return moves[random]
-}
+while(keepPlaying) {
 
-// player2 is the computer
-function checkWinner(player1, player2) {
-    let moveResult;
-    // check the moves
-    if (playerMove === computerMove) {
-        return 0;
-    }else{
-        if (playerMove === "rock" && computerMove === "paper") {
-            moveResult = -1;
-        } else {
-            if (playerMove === "scissors" && computerMove === "rock") {
+    // Moves
+    let playerMove = prompt("Type rock, paper, scissors");
+    let computerMove = randomMove();
+
+    function randomMove() {
+        let moves = ["rock", "paper", "scissors"]
+        let random = Math.floor(Math.random() * 2);
+        return moves[random]
+    }
+
+    // player2 is the computer
+    function checkWinner(player1, player2) {
+        let moveResult;
+        // check the moves
+        if (playerMove === computerMove) {
+            return 0;
+        }else{
+            if (playerMove === "rock" && computerMove === "paper") {
                 moveResult = -1;
             } else {
-                if (playerMove === "paper" && computerMove === "scissors") {
+                if (playerMove === "scissors" && computerMove === "rock") {
                     moveResult = -1;
                 } else {
-                    moveResult = 1;
+                    if (playerMove === "paper" && computerMove === "scissors") {
+                        moveResult = -1;
+                    } else {
+                        moveResult = 1;
+                    }
                 }
             }
         }
+        return moveResult;
     }
-    return moveResult;
+
+    let result = checkWinner(playerMove, computerMove);
+
+    alert(result);
+
+    keepPlaying = confirm("Do you want to keep playing?");
 }
-
-let result = checkWinner(playerMove, computerMove);
-
-alert(result);
 
 
